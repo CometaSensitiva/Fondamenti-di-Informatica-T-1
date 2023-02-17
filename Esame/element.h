55e37217d053d5288e49c64b289f04b407267cf1
CometaSensitiva <122211053+CometaSensitiva@users.noreply.github.com>
2023-02-17 22:35:37 +0100

    Add files via upload

---
+++ Esame/element.h
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#ifndef element_h
+#define element_h
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "lib.h"
+
+
+#endif
---
+++ Esame/main.c
@@ -0,0 +1,37 @@
+#include "funzioni.h"
+#include "list.h"
+
+int main() {
+
+	//Esercizio 1
+	printf("\tESERCIZIO 1\n\n");
+
+
+
+	printf("\n");
+
+	//Eserczio 2
+	printf("\n\n\tESERCIZIO 2\n\n");
+
+
+
+	printf("\n");
+
+	//Esercizio 3
+	printf("\n\n\tESERCIZIO 3\n\n");
+
+
+
+	printf("\n");
+
+	//Esercizio 4
+	printf("\n\n\tESERCIZIO 4\n\n");
+
+
+
+	printf("\n");
+
+	//DEALLOCA MEMORIA
+
+	return 0;
+}
