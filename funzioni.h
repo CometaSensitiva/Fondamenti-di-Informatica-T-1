27d8753bcfd3cc62e237323d6813a7528b847e85
CometaSensitiva <122211053+CometaSensitiva@users.noreply.github.com>
2023-02-17 22:32:12 +0100

    Add files via upload

---
+++ funzioni.h
@@ -0,0 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS
+#ifndef funzioni_h
+#define funzioni_h
+
+#include "element.h"
+
+#endif
---
+++ list.h
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+#ifndef list_h
+#define list_h
+
+#include "element.h"
+
+
+#endif
